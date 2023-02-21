f2519840a83f66be4b0e901b22230e5e1c5071ca
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-02-21 14:53:11 +0200

    redux files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
+import React from "react";
 import css from "./App.module.css";
 
 import ContactForm from "./components/ContactForm";
@@ -7,61 +6,19 @@ import Filter from "./components/Filter/Filter";
 import ContactsList from "./components/ContactList";
 import Container from "./components/Container";
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-  
-function App() {
-  const contactLS = JSON.parse(localStorage.getItem("contacts"));
-
-  const [contacts, setContacts] = useState(() => contactLS ?? initialContacts);
-  const [filter, setFilter] = useState("");
- 
-  
-  useEffect (() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = (name, number) => { 
-   contacts.every((contact) => contact.name.toLowerCase() !== name.toLowerCase())
-     ? setContacts([...contacts,
-       {
-         name,
-         number,
-         id: nanoid(),
-       },
-      ])
-      : alert(`${name} is already in contacts`);
-  };
-
- const deleteContact = (id) => {
-setContacts(contacts.filter((contact) => contact.id !== id));
-  };
-
- const handleFilterChange = (e) => { 
-    setFilter(e.currentTarget.value );
-  };
-
- const getFindContact = () => { 
-    const normalizeFilter = filter.toLowerCase();
-    return contacts.filter((contact) =>
-      contact.name.toUpperCase().includes(normalizeFilter));
-  };
+function App(){
     
     return (
       <div className={css.App}>
       <Container>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
+      <ContactForm />
       <h2>Contacts</h2>
-      <Filter value={filter} onFilterChange={handleFilterChange} />
-      <ContactsList list={getFindContact()} onClick={ deleteContact} />
+      <Filter />
+      <ContactsList />
       </Container>
       </div>
     );
-  }
+}
 
 export default App;
